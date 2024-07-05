b27826bea06e13c119c8d79d36e4d8944535e92e
anastasiaalapishvili <alapishvilianastasia@gmail.com>
2024-07-05 19:35:35 +0400

    first commit

---
+++ day 2/lesson2.py
@@ -0,0 +1,29 @@
+from turtle import *
+
+print("anastasia alapihvili")
+
+print("luka natrohvili")
+
+print("mate kotorashvili")
+
+print("mate kukutaria")
+
+print("nika kiknadze")
+
+print("nikoloz tsereteli")
+
+print("sandro esadze")
+
+print("tazo kidiashvili")
+
+print("tengo kimeridze")
+
+print("toma sepashvili-asatiani")
+
+print("datuna bregvadze")
+
+print("demetre areshidze")
+
+print("kesane bregvadze")
+
+exitonclick()
