193a0f7989cd3291f10f939db5948703c3df0c7c
anastasia alapishvili <alapishvilianastasia@gmail.com>
2024-07-08 14:03:40 +0400

    first commit

---
+++ day 3/lesson3.py
@@ -0,0 +1,23 @@
+from turtle import *
+
+print("giorgi mamardashvili")
+
+print("khvicha kvaratskhelia")
+
+print("giorgi chakvetadze")
+
+print("giorgi loria")
+
+print("zuriko davitashvili")
+
+print("natia levanishvili")
+
+print("mishka alapishvili")
+
+print("cira javakhishvili")
+
+print("nodari alapishvili")
+
+print("nodo alapishvili")
+
+print("tisia alapishvili")
